bc4d179750275efb2e877c27859b5cd2bff4c966
Tsvetelina Abadjieva <tsvetelina_abadjieva@abv.bg>
2017-06-04 23:43:48 +0300

    Add files via upload

---
+++ seachess/php/loadResults.php
@@ -0,0 +1,56 @@
+<?php
+//require_once "../common/header.php";
+require_once "../common/init.php";
+session_start();
+header("content-type:application/json");
+
+
+$url="game.php";
+
+$resultCollection= new ResultCollection();
+$total_rows=$resultCollection->getNumRows($_SESSION['username']);
+
+$per_page=10;
+$total_pages=(int)ceil($total_rows/$per_page);
+//$rest=$total_rows-($total_pages-1)*$per_page+1;
+
+$active_page= isset($_GET['page']) ? (int)htmlspecialchars(trim($_GET['page']),3):$total_pages;
+
+
+$matrix=isset($_GET['matrix'])?(int)htmlspecialchars(trim($_GET['matrix']),3):3;
+if($active_page<=1) {
+    $active_page=1;
+    $offset=0;
+}
+elseif($active_page>=$total_pages) {
+    $active_page=$total_pages;
+    $offset=$total_rows-$per_page;
+
+
+}
+else $offset=$total_rows-($total_pages-$active_page+1)*$per_page;
+
+$results=$resultCollection->getResults($offset,$per_page,$_SESSION['username']);
+//$results=$resultCollection->getResults(0,$per_page,$_SESSION['username']);
+
+//$data=json_encode($results);
+//echo $data;
+
+
+
+
+        foreach ($results as $key=>$result) {
+
+            echo '<tr>';
+            echo "<td>";
+                echo $result['id'];
+            echo '</td>';
+            echo '<td>';
+                echo $result['result'];
+            echo '</td>';
+            echo '</tr>';
+        }
+
+
+
+exit();
